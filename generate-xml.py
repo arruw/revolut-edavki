7ac73c84f2d9ad574924f07ade2e5cc731599988
Ziga Stegu <stegu.ziga@gmail.com>
2022-02-21 20:16:01 +0100

    CSV: Updated format to the one used at Revolut Stock Export.
    I used ";" as separator. It could be "," is default.
    Pandas uses "," by default.

--- generate-xml.py
+++ generate-xml.py
@@ -1,30 +1,31 @@
 import pandas as pd
+import numpy as np
 import xml.etree.ElementTree as ET
 from datetime import datetime
-from pprint import pprint
 import requests
 from lxml import etree
 import click
 import decimal as D
 
-import os, sys
+import os
 
 FOURPLACES = D.Decimal(10) ** -4
 
 def toDate(string):
-  return datetime.strptime(string, "%m/%d/%Y").date()
+  string = string[:10]
+  return datetime.strptime(string, "%d/%m/%Y").date()
 
 def dateFormat(date):
   return date.strftime("%Y-%m-%d")
 
-def getRate(date):
+def getRate(date, currency):
   if not os.path.exists(".cache/rates.xml"):
     req = requests.get("https://www.bsi.si/_data/tecajnice/dtecbs-l.xml", allow_redirects=True)
     os.mkdir(".cache")
     with open(".cache/rates.xml", "wb") as f: f.write(req.content)
 
   xml = etree.parse(".cache/rates.xml")
-  rate = xml.xpath("//x:tecajnica[@datum=$date]/x:tecaj[@oznaka='USD']/text()", namespaces={"x": "http://www.bsi.si"}, date=date)
+  rate = xml.xpath(f"//x:tecajnica[@datum=$date]/x:tecaj[@oznaka='{currency}']/text()", namespaces={"x": "http://www.bsi.si"}, date=date)
 
   return None if len(rate) == 0 else 1/D.Decimal(rate[0])
 
@@ -38,23 +39,28 @@ def toDecimal(x):
 @click.option("--input",      required=True, help="Revolut Activity CSV file")
 @click.option("--id",         required=True, help="TAX number")
 @click.option("--fullname",   required=True, help="Full Name")
-@click.option("--address",    required=True, help="Last Name")
+@click.option("--address",    required=True, help="Address")
 @click.option("--zip",        required=True, help="ZIP")
 @click.option("--city",       required=True, help="City")
 @click.option("--dob",        required=True, help="Date of birth YYYY-MM-DD")
 @click.option("--tel",        required=True, help="Telephone")
 @click.option("--email",      required=True, help="Email")
+def main():
+  main(input, id, fullname, address, zip, city, dob, tel, email)
+
 def main(input, id, fullname, address, zip, city, dob, tel, email):
   isindb = pd.read_csv("db/isin.csv")
 
-  activity = pd.read_csv(input, 
-    converters={"Quantity": toDecimal, "Price": toDecimal, "Amount": toDecimal}
-    )
-  activity["Trade Date"] = activity["Trade Date"].map(toDate)
-  activity["Settle Date"] = activity["Settle Date"].map(toDate)
-  del activity['Description']
-  del activity['Symbol / Description']
-  activity = activity[(activity['Activity Type'] == 'BUY') | (activity['Activity Type'] == 'SELL') | (activity['Activity Type'] == 'SSP')]
+  activity = pd.read_csv(
+    input, 
+    sep = ';',
+    converters={"Quantity": toDecimal,
+                "Price per share": toDecimal,
+                "Total Amount": toDecimal,
+                'Date': toDate,
+              }
+  )
+  activity = activity[(activity['Type'] == 'BUY') | (activity['Type'] == 'SELL')]
 
   root_el = ET.Element("Envelope", attrib={
     "xmlns": "http://edavki.durs.si/Documents/Schemas/Doh_KDVP_9.xsd",
@@ -85,25 +91,30 @@ def main(input, id, fullname, address, zip, city, dob, tel, email):
 
   # TODO
   ET.SubElement(kdvp_el, "DocumentWorkflowID").text = "O" # I or O
-  ET.SubElement(kdvp_el, "Year").text = "2020"
-  ET.SubElement(kdvp_el, "PeriodStart").text = "2020-01-01"
-  ET.SubElement(kdvp_el, "PeriodEnd").text = "2020-12-31"
+  ET.SubElement(kdvp_el, "Year").text = "2021"
+  ET.SubElement(kdvp_el, "PeriodStart").text = "2021-01-01"
+  ET.SubElement(kdvp_el, "PeriodEnd").text = "2021-12-31"
   ET.SubElement(kdvp_el, "IsResident").text = "true"
   ET.SubElement(kdvp_el, "TelephoneNumber").text = tel
-  ET.SubElement(kdvp_el, "SecurityCount").text = str(len(activity['Symbol'].unique()))
+  ET.SubElement(kdvp_el, "SecurityCount").text = str(len(activity['Ticker'].unique()))
   ET.SubElement(kdvp_el, "SecurityShortCount").text = "0"
   ET.SubElement(kdvp_el, "SecurityWithContractCount").text = "0"
   ET.SubElement(kdvp_el, "SecurityWithContractShortCount").text = "0"
   ET.SubElement(kdvp_el, "ShareCount").text = "0"
   ET.SubElement(kdvp_el, "Email").text = email
 
+  ticker_mapping = {
+    'VACQ': 'RKLB',
+  }
+  for key in ticker_mapping.keys():
+    activity['Ticker'] = activity['Ticker'].replace([key], ticker_mapping[key])
+
   row_id = 0
+  for symbol in activity['Ticker'].unique():
+    symbol_activity = activity[(activity['Ticker'] == symbol )]
 
-  for symbol in activity['Symbol'].unique():
-    symbol_activity = activity[(activity['Symbol'] == symbol)]
-
-    if len(symbol_activity[(symbol_activity["Quantity"] < 0)].index) == 0:
-      print(f"[WARN] Skipping {symbol} because there was no sells")
+    if 0 == len(symbol_activity[symbol_activity["Type"] == 'SELL']) :
+      print(f"[WARNING] Skipping {symbol} because there was no sells")
       continue
 
     kdvpitem_el = ET.SubElement(dohkdvp_el, "KDVPItem")
@@ -119,28 +130,34 @@ def main(input, id, fullname, address, zip, city, dob, tel, email):
     ET.SubElement(securities_el, "IsFond").text = "false"
 
     for index, sa in symbol_activity.iterrows():
-      date = dateFormat(sa["Settle Date"])
-      amount = sa["Quantity"].quantize(FOURPLACES)
-      priceUSD = sa["Price"]
-      rate = getRate(date)
-      priceEUR = (priceUSD * rate).quantize(FOURPLACES)
+      date = dateFormat(sa["Date"])
+      qty = sa["Quantity"].quantize(FOURPLACES)
+      price = sa["Price per share"]
+      if sa["Currency"] == 'EUR':
+        sharepriceEUR = price
+      else:
+        rate = getRate(date, sa["Currency"])
+        sharepriceEUR = (price * rate).quantize(FOURPLACES)
 
       row_el = ET.SubElement(securities_el, "Row")
       ET.SubElement(row_el, "ID").text = str(row_id)
       row_id += 1
 
-      if amount >= 0:
+      type = sa["Type"]
+      if type == "BUY":
         purchase_el = ET.SubElement(row_el, "Purchase")
         ET.SubElement(purchase_el, "F1").text = date
         ET.SubElement(purchase_el, "F2").text = "B" # nakup
-        ET.SubElement(purchase_el, "F3").text = str(amount)
-        ET.SubElement(purchase_el, "F4").text = str(priceEUR)
-      else:
+        ET.SubElement(purchase_el, "F3").text = str(qty)
+        ET.SubElement(purchase_el, "F4").text = str(sharepriceEUR)
+      elif type == "SELL":
         sale_el = ET.SubElement(row_el, "Sale")
         ET.SubElement(sale_el, "F6").text = date
-        ET.SubElement(sale_el, "F7").text = str(abs(amount))
-        ET.SubElement(sale_el, "F9").text = str(priceEUR)
+        ET.SubElement(sale_el, "F7").text = str(qty)
+        ET.SubElement(sale_el, "F9").text = str(sharepriceEUR)
         ET.SubElement(sale_el, "F10").text = "true"
+      else:
+        raise Exception("[ERROR] Filter for Transaction Types not working?")
 
   output = input.replace(".csv", ".xml")
   ET.ElementTree(root_el).write(output, encoding='utf8', method='xml')
